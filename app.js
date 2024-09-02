399fd6301a449b05125daeee0309ddb30e77873b
juan pablo <juanpablodelacruz16265@gmail.com>
2024-09-02 15:53:48 -0700

    practica1

---
+++ app.js
@@ -0,0 +1,15 @@
+import express, { json } from 'express' 
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
